45610e469f5fdfd4803bfec7b5bfab9a4158f484
AkhmadBG <akhmadbakir@yandex.ru>
2025-05-21 09:00:20 +0300

    Code-style

--- src/utils/AppConstants.java
+++ src/utils/AppConstants.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeFormatter;
 
 public final class AppConstants {
 
-    public final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd.MM.yyy");
-    public final static LocalDateTime DEFAULT_DATE_TO_EPIC_START = LocalDateTime.of(3000, Month.JANUARY, 1, 0, 0);
-    public final static LocalDateTime DEFAULT_DATE_TO_EPIC_END = LocalDateTime.of(1000, Month.JANUARY, 1, 0, 0);
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd.MM.yyy");
+    public static final LocalDateTime DEFAULT_DATE_TO_EPIC_START = LocalDateTime.of(3000, Month.JANUARY, 1, 0, 0);
+    public static final LocalDateTime DEFAULT_DATE_TO_EPIC_END = LocalDateTime.of(1000, Month.JANUARY, 1, 0, 0);
 
 }
